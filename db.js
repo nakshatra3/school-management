65ba44063d4c9eae47cdf78ff022ccb27adcdcbe
nakshatra3 <nakshatra4585@gmail.com>
2025-05-23 10:52:22 +0530

    db.js Commit

--- db.js
+++ db.js
@@ -1,32 +1,13 @@
-const mysql = require('mysql2/promise');
-const url = require('url');
+import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
 
-const connectionString = process.env.DATABASE_URL;
+dotenv.config();
 
-const dbConfig = (() => {
-  if (connectionString) {
-    const params = url.parse(connectionString);
-    const [user, password] = params.auth.split(':');
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
 
-    return {
-      host: params.hostname,
-      user: user,
-      password: password,
-      port: params.port,
-      database: params.pathname.split('/')[1]
-    };
-  } else {
-    // Fallback to individual env vars
-    return {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      port: process.env.DB_PORT
-    };
-  }
-})();
-
-const pool = mysql.createPool(dbConfig);
-
-module.exports = pool;
+export default pool;
